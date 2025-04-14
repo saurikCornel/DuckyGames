6f2f8977ddb232da2f14bb01d63bc91b83d783e3
Alex Belov <alex@cornel.su>
2025-04-14 19:42:06 +0300

    Initial Commit

---
+++ Ducky Games +/Ducky_Games__App.swift
@@ -0,0 +1,17 @@
+//
+//  Ducky_Games__App.swift
+//  Ducky Games +
+//
+//  Created by alex on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Ducky_Games__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ducky Games +Tests/Ducky_Games__Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Ducky_Games__Tests.swift
+//  Ducky Games +Tests
+//
+//  Created by alex on 4/14/25.
+//
+
+import Testing
+@testable import Ducky_Games__
+
+struct Ducky_Games__Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
